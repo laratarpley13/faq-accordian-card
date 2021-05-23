40f073ac806955d144da3aae579bf50c43999fb8
laratarpley13 <ltarp13@gmail.com>
2021-05-23 18:16:43 -0500

    initial uploads

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,18 @@
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for(i = 0; acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        let answer = this.nextElementSibling;
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    })
+}
