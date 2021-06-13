ebed8192feae3fcc2394df8a194d98f1cece1088
laratarpley13 <ltarp13@gmail.com>
2021-06-13 18:46:20 -0500

    style edits, html layout edits, adding functionality in js

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,18 +1,6 @@
-const acc = document.getElementsByClassName("accordion");
-let i;
-
-for(i = 0; acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        let answer = this.nextElementSibling;
-        if (answer.style.display === "block") {
-            answer.style.display = "none";
-        } else {
-            answer.style.display = "block";
-        }
+document.querySelectorAll('.accordion_question').forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.toggle('open');
+        item.nextElementSibling.classList.toggle('open');
     })
-}
+})
